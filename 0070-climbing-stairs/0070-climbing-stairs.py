a57f18956e6f74fd893b72e22dc3d39f27dae77e
Harry <quanghai.ng1512@gmail.com>
2023-09-19 15:36:04 +0700

    Time: 36 ms (73.61%), Space: 16.1 MB (98.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,12 @@
+import math
 class Solution:
     memo = {1: 1, 2: 2}
 
     def climbStairs(self, n: int) -> int:
-        if n in self.memo:
-            return self.memo[n]
-
-        self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-        return self.memo[n]
+        count = 1
+        num_two = n // 2
+        for i in range(num_two):
+            freq_two = i + 1
+            num_one = n - freq_two * 2
+            count += math.comb(freq_two + num_one, freq_two)
+        return count
